9403a583a6a3cf0517bcca1c9933661d6715729c
Ashwin Tirpude <ashwintirpude@C02G7D6VML7H.local>
2025-03-19 13:25:38 +0530

    stopped query caching

--- db/db.go
+++ db/db.go
@@ -25,7 +25,11 @@ func InitDatabase() *gorm.DB {
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require", host, user, password, database, port)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{PrepareStmt: false})
+	// db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{PrepareStmt: false})
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN:                  dsn,
+		PreferSimpleProtocol: true,
+	}), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
